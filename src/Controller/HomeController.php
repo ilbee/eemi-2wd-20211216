a9cf26cae7243c75411757a358b1ce98b743980f
Julien PRIGENT <julien.prigent@fnacdarty.com>
2021-12-16 11:29:18 +0100

    Create form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Entity\Movie;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,17 +16,28 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(EntityManagerInterface $em): Response
+    public function index(EntityManagerInterface $em, Request $request): Response
     {
         $movie = new Movie();
-        $movie->setName('The Matrix');
+        $form = $this
+            ->createFormBuilder($movie)
+            ->add('name', TextType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
 
-        $em->persist($movie);
-        $em->flush();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $movie = $form->getData();
+            $em->persist($movie);
+            $em->flush();
+
+        }
 
         return $this->render('home/index.html.twig', [
             'controller_name'   => 'HomeController',
-            'movie'             => $movie,
+            'formulaire'        => $form->createView(),
         ]);
     }
 }
