539342a5c2e80e5fcf7966becd55bb4bebcbac79
Julien PRIGENT <julien.prigent@fnacdarty.com>
2021-12-16 10:02:32 +0100

    Add entity "Movie"

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     * @Route("/coucou", name="home_new")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
